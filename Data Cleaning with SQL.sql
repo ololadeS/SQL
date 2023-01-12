896ba673cbc2e115ac20aa6e91034d80554be758
ololadeS <100509275+ololadeS@users.noreply.github.com>
2023-01-12 15:32:09 +0100

    Add files via upload

--- Data Cleaning with SQL.sql
+++ Data Cleaning with SQL.sql
@@ -5,51 +5,58 @@ SELECT * FROM Crime.[dbo].[crime]
 
 
 -- Get the number of rows 
-SELECT count (*) FROM Crime.[dbo].[crime]
+SELECT count(*) FROM Crime.[dbo].[crime]
 
--- Count the mumber of values for shooting 
+
+-- Identify and remove duplicates using CTE, ROW_NUMBER, PARTITION BY 
+WITH CTE AS 
+(
+SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY 
+                Incident_number
+            ORDER BY 
+                Incident_number ) AS row_num
+FROM Crime.[dbo].[crime] 
+)
+DELETE FROM CTE
+WHERE row_num > 1
+
+
+-- Count the mumber of values for shooting  
 SELECT year, count (shooting) AS PositionCount
 FROM Crime.[dbo].[crime]
+WHERE SHOOTING IS NOT NULL
 GROUP BY YEAR
 
-	SELECT count(*) - count(shooting), count(shooting) FROM Crime.[dbo].[crime]
+SELECT count(*) - count(shooting), count(shooting) FROM Crime.[dbo].[crime]
 
 
---Populate street, district and reporting area columns to get isnull
-	-- Check missong values for street, district and reporting area columns
-		SELECT * FROM Crime.[dbo].[crime]
-		WHERE street is null AND district is null AND reporting_area IS NULL
-		ORDER BY year
+-- Replacing shooting column null values with U (i.e unknown):
+UPDATE Crime.[dbo].[crime]
+SET shooting = 'U'
+WHERE shooting IS NULL;
+
+--Find and replace value i.e y and u to yes and unknown using CASE STATEMENT
+SELECT DISTINCT (shooting), COUNT (shooting) Sold_Count
+FROM Crime.[dbo].[crime]
+GROUP BY (shooting) 
+ORDER BY 2
+
+SELECT shooting,
+CASE WHEN shooting = 'Y' THEN 'Yes'
+	WHEN shooting = 'U' THEN 'Unknown'
+	ELSE shooting END
+FROM Crime.[dbo].[crime]
+
+UPDATE [crime]
+SET Shooting  =
+CASE WHEN shooting = 'Y' THEN 'Yes'
+	WHEN shooting = 'U' THEN 'Unknown'
+	ELSE shooting END
+FROM Crime.[dbo].[crime]
 
 
-		-- count
-		SELECT year, count (*) FROM Crime.[dbo].[crime]
-		WHERE street IS NULL AND district IS NULL AND reporting_area IS NULL
-		GROUP BY year
-
-
-SELECT I.incident_number, II.incident_number, I.street, II.street, I.district, II.district, I.reporting_area, II.reporting_area
-ISNULL ((I.street, II.street) AS Populatestreet, (I.district, II.district) AS Populatedistrict, (I.reporting_area, II.reporting_area) AS PopulateR_area)
-FROM Crime.[dbo].[crime]  I
-JOIN Crime.[dbo].[crime] II
-	ON I.incident_number = II.incident_number
-	AND I.[UniqueID ]<> II.[UniqueID ]
-WHERE I.street IS NULL AND I.district is null AND I.reporting_area IS NULL
-
-
-UPDATE I
-SET street = ISNULL (I.street, II.street) 
-SET district = ISNULL (I.district, II.district)
-SET reporting_area = ISNULL (I.reporting_area, II.reporting_area)
-FROM Crime.[dbo].[crime]  I
-JOIN Crime.[dbo].[crime] II
-	ON I.incident_number = II.incident_number
-	AND I.[UniqueID ]<> II.[UniqueID ]
-WHERE I.street IS NULL AND I.district is null AND I.reporting_area IS NULL
-
-
-
---Split Location column into individual columns i.e latitude and Longitude using CHARINDEX
+--Split Location column into individual columns i.e latitude and Longitude using substring and CHARINDEX
 SELECT location FROM Crime.[dbo].[crime]
 
 SELECT 
@@ -59,41 +66,31 @@ FROM Crime.[dbo].[crime]
 
 
 ALTER TABLE Crime.[dbo].[crime]
-ADD SplitLatitude NVARCHAR (255);
-
+ADD Latitude NVARCHAR (255);
 UPDATE Crime.[dbo].[crime]
-SET SplitLatitude = SUBSTRING (location, 1, CHARINDEX (' ', location)-1)
+SET Latitude = SUBSTRING (location, 1, CHARINDEX (' ', location)-1)
 
 
 ALTER TABLE Crime.[dbo].[crime]
-ADD SplitLongitude NVARCHAR (255);
+ADD Longitude NVARCHAR (255);
+UPDATE Crime.[dbo].[crime]
+SET Longitude = SUBSTRING (location, CHARINDEX (' ', location)+1, LEN(location));
+
+
+--Removing leading and trailing whitespaces:
+UPDATE Crime.[dbo].[crime]
+SET Latitude = LTRIM(RTRIM(Latitude));
 
 UPDATE Crime.[dbo].[crime]
-SET SplitLongitude = SUBSTRING (location, CHARINDEX (' ', location)+1, LEN(location));
-
-SELECT * FROM Crime.[dbo].[crime]
+SET Longitude = LTRIM(RTRIM(Longitude));
 
 
---Split occurred_on_date using PARSENAME 
-SELECT 
-PARSENAME (REPLACE (occurred_on_date, ' ', ' ' ), 2) Datetime, 
-PARSENAME (REPLACE (occurred_on_date, '', ' ' ), 1) Hours 
-FROM Crime.[dbo].[crime]
-
-
-ALTER TABLE Crime.[dbo].[crime]
-ADD SplitDatetime date;
-
+-- Removing special characters:
+UPDATE Crime.[dbo].[crime]
+SET Latitude = REPLACE(Latitude, '(', '');
 
 UPDATE Crime.[dbo].[crime]
-SET SplitDatetime = PARSENAME (REPLACE (occurred_on_date, ' ', ' '), 2) 
-
-
-ALTER TABLE Crime.[dbo].[crime]
-ADD SplitHours date;
-
-UPDATE Crime.[dbo].[crime]
-SET SplitHours = PARSENAME (REPLACE (occurred_on_date, ' ', ' '), 1) 
+SET Longitude = REPLACE(Longitude, ')', '');
 
 
 --Standardize date format
@@ -108,59 +105,65 @@ SET occurred_on_date =  CONVERT(Date,occurred_on_date)
 	UPDATE Crime.[dbo].[crime]
 	SET occurred_on_date = CONVERT(Date,occurred_on_date)
 
-
-
-
---Find and replace value i.e y and null to yes and unknown using CASE STATEMENT
-SELECT DISTINCT (shooting), COUNT (shooting) Sold_Count
-FROM Crime.[dbo].[crime]
-GROUP BY (shooting) 
-ORDER BY 2
-
-SELECT shooting,
-CASE WHEN shooting = 'Y' THEN 'Yes'
-	WHEN shooting IS NULL THEN 'Unknown'
-	ELSE shooting END
-FROM Crime.[dbo].[crime]
-
-UPDATE [crime]
-SET Shooting  =
-CASE WHEN shooting = 'Y' THEN 'Yes'
-	WHEN shooting IS NULL THEN 'Unknown'
-	ELSE shooting END
+--Split occurred_on_date using PARSENAME 
+SELECT 
+PARSENAME (REPLACE (occurred_on_date, ' ', ' ' ), 2) DATE_, 
+PARSENAME (REPLACE (occurred_on_date, ' ', ' ' ), 1) Hours 
 FROM Crime.[dbo].[crime]
 
 
---Remove duplicates using CTE, ROW_NUMBER, PARTITION BY 
-WITH crimeCTE AS 
-(
-SELECT *,
-        ROW_NUMBER() OVER (PARTITION BY 
-                Incident_number
-            ORDER BY 
-                Incident_number ) AS row_num
-FROM Crime.[dbo].[crime] 
-)
-DELETE FROM crimeCTE
-WHERE row_num > 1
+ALTER TABLE Crime.[dbo].[crime]
+ADD Date_ date;
+UPDATE Crime.[dbo].[crime]
+SET Date_ = PARSENAME (REPLACE (occurred_on_date, ' ', ' '), 2) 
 
 
--- --Join two Tables (Crime and offense code) using inner join 
-SELECT * FROM Crime.[dbo].[crime] AS crime
-INNER Join Crime.[dbo].[offense_codes] AS code
-	ON crime.offense_code = crime.offense_code
-	AND code.Name = code.Name
+ALTER TABLE Crime.[dbo].[crime]
+ADD Hours_ date;
+UPDATE Crime.[dbo].[crime]
+SET Hours_ = PARSENAME (REPLACE (occurred_on_date, ' ', ' '), 1) 
 
 
---Delete unused columns using DROP
+--Populate street, district and reporting area columns to get isnull
+	-- Check missng values for street, district and reporting area columns
+		SELECT * FROM Crime.[dbo].[crime]
+		WHERE street is null AND district is null AND reporting_area IS NULL
+		ORDER BY year
+
+		-- count
+		SELECT year, count (*) FROM Crime.[dbo].[crime]
+		WHERE street IS NULL AND district IS NULL AND reporting_area IS NULL
+		GROUP BY year;
+
+
+-- Identifying and Updating incorrect data:
+UPDATE Crime.[dbo].[crime]
+SET District = 'Unknown'
+WHERE District IS NULL;
+
+-- Identifying and Updating incorrect data:
+UPDATE Crime.[dbo].[crime]
+SET STREET = 'Unknown'
+WHERE STREET IS NULL;
+
+-- Removing rows with invalid data:
+DELETE FROM  Crime.[dbo].[crime]
+WHERE STREET IS NULL OR REPORTING_AREA IS NULL OR OFFENSE_CODE < 0;
+
+-- Changing the case of a column:
+UPDATE Crime.[dbo].[crime]
+SET OFFENSE_DESCRIPTION = STUFF(LOWER(OFFENSE_DESCRIPTION), 1, 1, UPPER(LEFT(OFFENSE_DESCRIPTION, 1)))
+
+
+-- Removing duplicate rows:
+DELETE FROM Crime.[dbo].[crime]
+WHERE INCIDENT_NUMBER NOT IN (SELECT MIN(INCIDENT_NUMBER) FROM Crime.[dbo].[crime] GROUP BY OFFENSE_CODE);
+
+-- Removing unwanted columns:
 SELECT * FROM Crime.[dbo].[crime]
 
 ALTER TABLE Crime.[dbo].[crime]
-DROP COLUMN Lat, Long
+DROP COLUMN Hours_, occurred_on_dateconverted
 
 
-
--- What types of crimes are most common?
--- Where are different types of crimes most likely to occur?
--- Does the frequency of crimes change over the day? Week? Year?
 
